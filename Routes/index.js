5cea9f45f27a865308c125595e857f8c5d7fae2c
iic-sydney <iic_sydney@au1.ibm.com>
2017-01-27 10:32:53 +1100

    commit

--- Routes/index.js
+++ Routes/index.js
@@ -4,6 +4,6 @@ var router = express.Router()
 
 // respond with "hello world" when a GET request is made to the homepage
 router.get('/', function (req, res, next) {
-  res.render(index, {title : "Watson Demonstration"})
+  res.render('index', {title : "Watson Demonstration"})
 })
 module.exports = router
